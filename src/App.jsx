49d7f35643086ff83e3e5e206d7e588b2771883d
Cotchi666 <chien.170901@gmail.com>
2023-11-24 15:40:18 +0700

    select theme init

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,55 @@
 import { useColorScheme } from '@mui/material/styles'
-import Button from '@mui/material/Button'
-
-function ModeToggle() {
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+function ModeSelect() {
   const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
   return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize="small" /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <DarkModeOutlinedIcon fontSize="small" />
+            Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="system">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <SettingsBrightnessIcon fontSize="small" />
+            System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
+
 
 function App() {
   return (
     <>
-      <ModeToggle />
+      <ModeSelect />
     </>
   )
 }
