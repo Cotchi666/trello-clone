acfdd927d30789d9a0a60a79ce6aa9cef2ad17db
Cotchi666 <chien.170901@gmail.com>
2023-11-25 18:15:08 +0700

    update-ui-home-page

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ import Select from '@mui/material/Select'
 import LightModeIcon from '@mui/icons-material/LightMode'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
-
+ 
   const handleChange = (event) => {
     setMode(event.target.value)
   }
@@ -45,12 +48,38 @@ function ModeSelect() {
   )
 }
 
-
 function App() {
   return (
-    <>
-      <ModeSelect />
-    </>
+    <Container
+      disableGutters
+      maxWidth={false}
+      sx={{ height: '100vh' }}
+    >
+      <Box
+        sx={{
+          backgroundColor: 'primary.light',
+          width: '100%',
+          height: (theme) => theme.trello.appBarHeight,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        <ModeSelect />
+      </Box >
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>Board Bar</Box>
+      <Box
+        sx={{ backgroundColor: 'primary.main',
+          width:'100%',
+          height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+          display: 'flex',
+          alignItems: 'center' }}> Board Content</Box>
+    </Container>
   )
 }
 
