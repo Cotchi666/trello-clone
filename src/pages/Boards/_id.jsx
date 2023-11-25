2d071c8f8b6e845ffed55a5168f61e3bf4b61269
Cotchi666 <chien.170901@gmail.com>
2023-11-25 18:59:27 +0700

    hot-fix import

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,5 +1,5 @@
 import Container from '@mui/material/Container'
-import AppBar from '../../components/AppBar/AppBar'
+import AppBar from '../../components/AppBar'
 import BoardContent from './BoardContent'
 import BoardBar from './BoardBar'
 function Board() {
