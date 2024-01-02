5c72ad69036767dcbaab750f281bd9270c3cc3ef
Cotchi666 <chien.170901@gmail.com>
2024-01-02 11:47:40 +0700

    create workspaces and recent menu

--- src/components/AppBar/Menus/Workspaces.jsx
+++ src/components/AppBar/Menus/Workspaces.jsx
@@ -3,6 +3,15 @@ import Button from '@mui/material/Button'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Typography from '@mui/material/Typography'
+import ContentCut from '@mui/icons-material/ContentCut'
+import ContentCopy from '@mui/icons-material/ContentCopy'
+import ContentPaste from '@mui/icons-material/ContentPaste'
+import Cloud from '@mui/icons-material/Cloud'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 function Workspaces() {
   const [anchorEl, setAnchorEl] = React.useState(null)
   const open = Boolean(anchorEl)
@@ -21,8 +30,9 @@ function Workspaces() {
         aria-haspopup="true"
         aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
+        end-icon={<ExpandMoreIcon />}
       >
-        Dashboard
+        Workspaces
       </Button>
       <Menu
         id="basic-menu-workspaces"
@@ -33,9 +43,40 @@ function Workspaces() {
           'aria-labelledby': 'basic-button-workspaces'
         }}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <ContentCut fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Cut</ListItemText>
+          <Typography variant="body2" color="text.secondary">
+            ⌘X
+          </Typography>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <ContentCopy fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Copy</ListItemText>
+          <Typography variant="body2" color="text.secondary">
+            ⌘C
+          </Typography>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <ContentPaste fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Paste</ListItemText>
+          <Typography variant="body2" color="text.secondary">
+            ⌘V
+          </Typography>
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <Cloud fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Web Clipboard</ListItemText>
+        </MenuItem>
       </Menu>
     </Box>
   )
--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -4,24 +4,41 @@ import SvgIcon from '@mui/material/SvgIcon'
 import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
 import AppsIcon from '@mui/icons-material/Apps'
 import Typography from '@mui/material/Typography'
+import Workspaces from './Menus/Workspaces'
+import Recent from './Menus/Recent'
 function AppBar() {
   return (
     <Box
       px={2}
       sx={{
         width: '100%',
-        height: (theme) => theme.trello.appBarHeight,
+        height: theme => theme.trello.appBarHeight,
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'space-between'
       }}
     >
-      <Box sx={{ display: 'flex', alignItems: 'center', gap:2 }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }} />
-        <Box sx={{ display: 'flex', alignItems: 'center', gap:0.5 }}>
-          <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'primary.main' }}  />
-          <Typography sx={{fontsize: '1.2rem', color: 'primary.main' , fontWeight: 'bold'}} variant="span">Trello</Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon
+            component={TrelloIcon}
+            inheritViewBox
+            sx={{ color: 'primary.main' }}
+          />
+          <Typography
+            sx={{
+              fontsize: '1.2rem',
+              color: 'primary.main',
+              fontWeight: 'bold'
+            }}
+            variant="span"
+          >
+            Trello
+          </Typography>
         </Box>
+        <Workspaces />
+        <Recent />
       </Box>
       <Box>
         <ModeSelect />
