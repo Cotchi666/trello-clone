955f201f3616c104732151ea964730c95bff049b
Cotchi666 <chien.170901@gmail.com>
2024-01-04 11:38:49 +0700

    update board bar chip item menu

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,18 +1,73 @@
 import Box from '@mui/material/Box'
-
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+const MENU_STYLES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 function BoardBar() {
   return (
     <Box
+      px={2}
       sx={{
-        backgroundColor: 'primary.light',
         width: '100%',
-        height: (theme) => theme.trello.appBarHeight,
+        height: theme => theme.trello.appBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+
+        overflowX: 'auto',
+        borderTop: '1px solid #00bfa5'
       }}
     >
-        Board Bar
-    </Box> )
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={{ MENU_STYLES }}
+          icon={<DashboardIcon />}
+          label="Chien MERN Stack Board"
+          clickable
+        />
+        <Chip
+          sx={{
+            MENU_STYLES
+          }}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspaces"
+          clickable
+        />
+        <Chip
+          sx={{
+            MENU_STYLES
+          }}
+          icon={<AddToDriveIcon />}
+          label="Add to Google Drive"
+          clickable
+        />
+        <Chip
+          sx={{
+            MENU_STYLES
+          }}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+        />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}></Box>
+    </Box>
+  )
 }
 
 export default BoardBar
