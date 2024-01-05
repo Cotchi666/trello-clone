1859e943f4730992c61af33b05d5d4c433f6c0b5
Cotchi666 <chien.170901@gmail.com>
2024-01-05 14:33:31 +0700

    init box board content

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -10,11 +10,24 @@ function BoardContent() {
         width: '100%',
         height: theme =>
           `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-        display: 'flex',
-        alignItems: 'center'
+        display: 'flex'
       }}
     >
-      Board Content
+      <Box
+        sx={{
+          minWidth: '300px',
+          maxWidth: '300px',
+          bgcolor: theme =>
+            theme.palette.mode === 'dark' ? '#333643' : '#ebecf0',
+          ml: 2,
+          borderRadius: '6px'
+        }}
+      >
+        <Box sx={{}}>Header</Box>
+        <Box sx={{}}>List Card</Box>
+
+        <Box sx={{}}>Footer</Box>
+      </Box>
     </Box>
   )
 }
