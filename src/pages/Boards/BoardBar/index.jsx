73b14bd2457a67a194db901efce25b9ba4f0ff13
Cotchi666 <chien.170901@gmail.com>
2024-01-05 14:24:30 +0700

    update color avatar group

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -94,7 +94,12 @@ function BoardBar() {
               width: 34,
               height: 34,
               fontSize: 16,
-              border: 'none'
+              border: 'none',
+              color: 'white',
+              cursor: 'pointer',
+              '&:first-of-type': {
+                bgcolor: '#a4b0be'
+              }
             }
           }}
         >
