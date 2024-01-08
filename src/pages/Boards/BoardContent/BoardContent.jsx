0635d16069bc4b9da3921f48e78c8b24322a8d24
Cotchi666 <chien.170901@gmail.com>
2024-01-08 10:13:49 +0700

    update drag drop

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,15 +1,49 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utlis/sorts'
-import { DndContext } from '@dnd-kit/core'
+import {
+  DndContext,
+  PointerSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { useEffect, useState } from 'react'
+
 function BoardContent({ board }) {
-  const orderedColumn = mapOrder(board?.columns, board?.columnOrderIds, '_id')
-  const handleDragEnd = e => {
-    console.log('handleDragEnd', e)
+  const pointerSensor = useSensor(PointerSensor, {
+    // click on title string
+    activationConstraint: { distance: 10 }
+  })
+  const sensors = useSensors(pointerSensor)
+
+  // current columns
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  // fetch data
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  // handle drag
+  const handleDragEnd = event => {
+    // active: old position || over: new position
+    const { active, over } = event
+    // over null
+    if (!over) return
+    // position changes
+    if (active.id !== over.id) {
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+      // const orderedColumnsIds = orderedColumns.map(c => c._id);
+      // return arrayMove(orderedColumns, oldIndex, newIndex);
+      // state client update
+      setOrderedColumns(arrayMove(orderedColumns, oldIndex, newIndex))
+    }
   }
 
   return (
-    <DndContext onDragEnd={handleDragEnd}>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
       <Box
         sx={{
           bgcolor: theme =>
@@ -19,7 +53,7 @@ function BoardContent({ board }) {
           p: '10px 0'
         }}
       >
-        <ListColumns columns={orderedColumn} />
+        <ListColumns columns={orderedColumns} />
       </Box>
     </DndContext>
   )
