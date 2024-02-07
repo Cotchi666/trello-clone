5299615e7bbbd8d87f9cc0783dfd716e138152c4
Cotchi666 <chien.170901@gmail.com>
2024-02-07 09:02:59 +0700

    update env varible

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -3,3 +3,7 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+const MONGODB_URI = 'mongodb+srv://sa:sa@cluster0.vadgnss.mongodb.net/?retryWrites=true&w=majority'
+
+const DATABASE_NAME = 'trello-db'
