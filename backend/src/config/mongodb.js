862cd00b4dfc4455309d2c9302a6ef8a61393db3
Cotchi666 <chien.170901@gmail.com>
2024-02-07 09:08:34 +0700

    update mongo Client

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -1,9 +1,16 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
-const MONGODB_URI = 'mongodb+srv://sa:sa@cluster0.vadgnss.mongodb.net/?retryWrites=true&w=majority'
+const MONGODB_URI =
+  'mongodb+srv://sa:sa@cluster0.vadgnss.mongodb.net/?retryWrites=true&w=majority'
 
 const DATABASE_NAME = 'trello-db'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
