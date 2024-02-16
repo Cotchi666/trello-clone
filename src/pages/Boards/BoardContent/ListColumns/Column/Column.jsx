bd79ac0eb6861a6f738dcb7b771c183e8a77eed5
Cotchi <=>
2024-02-16 15:35:12 +0700

    update fix

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -211,6 +211,7 @@ function Column({ column }) {
                 type="text"
                 variant="outlined"
                 autoFocus
+                data-no-dnd="true"
                 value={newCardTitle}
                 onChange={e => setNewCardTitle(e.target.value)}
                 sx={{
@@ -228,6 +229,7 @@ function Column({ column }) {
               />
               <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
                 <Button
+                  data-no-dnd="true"
                   onClick={addNewCard}
                   variant="contained"
                   color="success"
