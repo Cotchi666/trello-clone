4a72f77da8e5e9474af96ef74f1412f1adfbc862
Cotchi <=>
2024-02-17 10:49:01 +0700

    combile apis

--- src/apis/index.js
+++ src/apis/index.js
@@ -8,11 +8,12 @@ export const fetchBoardDetailsAPI = async boardId => {
 }
 // Columns
 export const createNewColumnAPI = async newColumnData => {
-  const response = await axios.get(`${API_ROOT}/v1/columns/${newColumnData}`)
+  console.log('new column data', newColumnData)
+  const response = await axios.post(`${API_ROOT}/v1/columns`, newColumnData)
   return response.data
 }
 // Cards
 export const createNewCardAPI = async newCardData => {
-  const response = await axios.get(`${API_ROOT}/v1/cards/${newCardData}`)
+  const response = await axios.post(`${API_ROOT}/v1/cards`, newCardData)
   return response.data
 }
